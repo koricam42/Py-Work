a4ec7989d3975b59e76f7fbf91aae8de352b88cd
Jordan Mensah <koricam42@gmail.com>
2026-09-21 23:03:14 -0400

    Add ten and twenty sided die, made each for loop print out as many numbers as each die has sides according to video instructions

--- participation-stuff/major.py
+++ participation-stuff/major.py
@@ -13,12 +13,21 @@ class Die:
     def __init__(self, sides=6):
         self.sides = sides
 
-    
+
     def roll_die(self):
         roll = randint(1, self.sides) 
         print(roll)
 
 six_sided_die = Die()
-for x in range(10):
+for x in range(6):
     six_sided_die.roll_die()
+
+ten_sided_die = Die(10)
+for x in range(10):
+    ten_sided_die.roll_die()
+
+twenty_sided_die = Die(20)
+for x in range(20):
+    twenty_sided_die.roll_die()
+
 
