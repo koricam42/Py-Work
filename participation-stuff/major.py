20261eaf4914a2cfe23f0b78572366aa7d1acdc7
Jordan Mensah <koricam42@gmail.com>
2026-09-21 20:59:19 -0400

    Start weekly participation work with header docstring and die class

---
+++ participation-stuff/major.py
@@ -0,0 +1,15 @@
+"""
+Program Name: Dice Roll
+Author: Jordan Mensah
+Purpose: Python-based program rolls a die
+Starter Code: 
+ - Instructions/General Help: https://learning.oreilly.com/library/view/python-crash-course/9781098156664/c09.xhtml#h1-502703c09-0001
+Date: 9/21/26
+"""
+
+
+class Die:
+
+    def roll_die():
+
+        pass
